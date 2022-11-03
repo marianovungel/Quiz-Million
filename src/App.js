de16f054dd469cb3eda50930be13af31cde388f2
marianovungel <vungem88@gmail.com>
2022-11-03 11:59:09 -0300

    Quiz game

---
+++ src/App.js
@@ -0,0 +1,160 @@
+import { useEffect, useMemo, useState } from "react";
+import "./App.css"
+import Start from "./Componets/Start";
+import Timer from "./Componets/Timer";
+import Trivia from "./Componets/Trivia";
+
+function App() {
+
+  const [questionNumber, setQuestionNumber] = useState(1)
+  const [stop, setStop] = useState(false)
+  const [username, setUsername] = useState(null)
+  const [earner, setEarner] = useState("$ 0.00")
+
+  const newPlay =()=>{
+    setQuestionNumber(1)
+    setStop(false)
+  }
+
+  const data = [
+    {
+      id: 1,
+      question: "Rolex is a company that specializes in what type of product?",
+      answers: [
+        {
+          text: "Phone",
+          correct: false,
+        },
+        {
+          text: "Watches",
+          correct: true,
+        },
+        {
+          text: "Food",
+          correct: false,
+        },
+        {
+          text: "Cosmetic",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 2,
+      question: "When did the website `Facebook` launch?",
+      answers: [
+        {
+          text: "2004",
+          correct: true,
+        },
+        {
+          text: "2005",
+          correct: false,
+        },
+        {
+          text: "2006",
+          correct: false,
+        },
+        {
+          text: "2007",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 3,
+      question: "Who played the character of harry potter in movie?",
+      answers: [
+        {
+          text: "Johnny Deep",
+          correct: false,
+        },
+        {
+          text: "Leonardo Di Caprio",
+          correct: false,
+        },
+        {
+          text: "Denzel Washington",
+          correct: false,
+        },
+        {
+          text: "Daniel Red Cliff",
+          correct: true,
+        },
+      ],
+    },
+  ];
+
+  const moneyPyramid = useMemo(()=>
+    [
+      {id:1, amount:"$100"},
+      {id:2, amount:"$200"},
+      {id:3, amount:"$300"},
+      {id:4, amount:"$500"},
+      {id:5, amount:"$1000"},
+      {id:6, amount:"$2000"},
+      {id:7, amount:"$4000"},
+      {id:8, amount:"$8000"},
+      {id:9, amount:"$16000"},
+      {id:10, amount:"$32000"},
+      {id:11, amount:"$64000"},
+      {id:12, amount:"$125000"},
+      {id:13, amount:"$250000"},
+      {id:14, amount:"$500000"},
+      {id:15, amount:"$1000000"},
+    ].reverse(),
+ [])
+
+  useEffect(()=>{
+    questionNumber > 1 && setEarner(moneyPyramid.find(m=> m.id === questionNumber - 1).amount)
+  }, [moneyPyramid, questionNumber])
+  return (
+    <div className="app">
+      {username ? (
+        <>
+          <div className="main">
+        {stop ? (
+          <>
+            <h1 className="endText">You earner: {earner}</h1>
+            <button className="playagain" onClick={newPlay}>Play Again</button>
+          </>
+        ) : (
+          <>
+            <div className="top">
+              <div className="timer">
+                <Timer 
+                  setStop={setStop}
+                  questionNumber={questionNumber}
+                />
+              </div>
+            </div>
+            <div className="bottom">
+              <Trivia 
+                data={data} 
+                setStop={setStop} 
+                questionNumber={questionNumber}
+                setQuestionNumber={setQuestionNumber}
+                />
+            </div>
+          </>
+        )}
+      </div>
+      <div className="pyramid">
+        <ul className="moneyList">
+          {moneyPyramid.map((m)=>(
+            <li className={questionNumber === m.id ? "moneyListItem active" : "moneyListItem"}>
+              <span className="moneyListItemNumber">{m.id}</span>
+              <span className="moneyListItemAmount">{m.amount}</span>
+            </li>
+          ))}
+          
+        </ul>
+      </div> 
+        </>
+      ): <Start setUsername={setUsername} />}
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Componets/Start.jsx
@@ -0,0 +1,16 @@
+import { useRef } from "react"
+
+export default function Start({setUsername}) {
+
+    const inputRef = useRef()
+
+    const handleClick = ()=>{
+        inputRef.current.value && setUsername(inputRef.current.value)
+    }
+  return (
+    <div className="start">
+        <input ref={inputRef} type="text" placeholder="Enter you Name" className="startInput"/>
+        <button className="startButton" onClick={handleClick}>Start</button>
+    </div>
+  )
+}
